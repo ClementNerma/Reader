a74e807d1c1974c0d9329735564c6fb10f01e226
Clément Nerma <clement.nerma@gmail.com>
2023-08-08 12:04:41 +0200

    Don't exit silently when no path is provided

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,28 @@
 // Don't display terminal when launching the program on Windows
 #![windows_subsystem = "windows"]
 
+use std::path::PathBuf;
+
+use anyhow::anyhow;
 // Required for image decoding support
 use image as _;
 
-mod cmd;
 mod gap_vec;
 mod img_sources;
 mod settings;
 mod ui;
 
-use clap::Parser;
 use eframe::NativeOptions;
 
-use self::{
-    cmd::Args,
-    ui::{app::ReaderApp, show_err_dialog},
-};
+use self::ui::{app::ReaderApp, show_err_dialog};
 
 fn main() -> eframe::Result<()> {
-    let Args {
-        path,
-        // double_page,
-        // right_to_left,
-    } = Args::parse();
+    let Some(path) = std::env::args().nth(1) else {
+        show_err_dialog(anyhow!("Please open a comic with this program"));
+        std::process::exit(1);
+    };
+
+    let path = PathBuf::from(path);
 
     eframe::run_native(
         "reader",
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -5,6 +5,7 @@ pub mod app;
 pub fn show_err_dialog(err: anyhow::Error) {
     MessageDialog::new()
         .set_level(MessageLevel::Error)
+        .set_title("Error")
         .set_description(&format!("{err:?}"))
         .show();
 }
