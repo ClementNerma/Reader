c634c1a15abe7b7e883c40be6e5ccf6f4bd72b78
Clément Nerma <clement.nerma@gmail.com>
2023-08-10 21:17:59 +0200

    Comment the application + change a few things

--- src/settings.rs
+++ src/settings.rs
@@ -1,8 +1,18 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Settings {
     pub right_to_left: bool,
     pub double_page: bool,
     pub display_pages_number: bool,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            right_to_left: false,
+            double_page: false,
+            display_pages_number: true,
+        }
+    }
 }
--- src/ui/app.rs
+++ src/ui/app.rs
@@ -22,32 +22,50 @@ use crate::{
 type PageLoadingResult = Result<(PathBuf, Vec<u8>), String>;
 
 pub struct ReaderApp {
+    /// [`egui`]'s context
     ctx: Context,
 
+    /// All threads used by the application
     thread_handles: Vec<JoinHandle<()>>,
+
+    /// Setting this signal to `true` will make all the thread stop properly
+    /// This allows them to properly finish their work and quit in a non-dirty state
     threads_stop_signal: Arc<AtomicBool>,
     
+    /// Application settings
     settings: Arc<RwLock<Settings>>,
 
+    /// Path of the currently opened file or directory (None = no file is opened)
     path: Option<PathBuf>,
+
+    /// Total number of pages in the current file
     total_pages: usize,
 
+    /// All loaded pages (as bytes)
     loaded_pages: Arc<RwLock<GapVec<PageLoadingResult>>>,
 
+    // This is used to allow a rendering closure to store result of the only two
+    // pages we may be interested in: the left and right one (in double mode)
+    //
+    // When the computable image is displayed, we store it here to avoid having to
+    // re-compute it on each frame
     retained_odd_page_image: RefCell<Option<(usize, TextureHandle, Vec2)>>,
     retained_even_page_image: RefCell<Option<(usize, TextureHandle, Vec2)>>,
 
+    /// Current page number
     current_page: Arc<AtomicUsize>,
 
+    /// Contains the "jump to page" modal's prompt (if opened)
     page_prompt: Option<String>,
 }
 
 impl ReaderApp {
+    /// Set up the application
     pub fn new(
         cc: &eframe::CreationContext<'_>,
         path: Option<PathBuf>,
-        // settings: Settings,
     ) -> Result<Self> {
+        // Load settings from the application's storage, or use default ones
         let settings = match cc.storage {
             Some(storage) => eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default(),
             None => Settings::default(),
@@ -57,6 +75,7 @@ impl ReaderApp {
             cc.egui_ctx.clone(),
             match path {
                 Some(ref path) => load_image_source(path)?,
+                // If no path was provided, load a dummy empty source
                 None => Box::new(EmptySource::new())
             },
             path,
@@ -64,6 +83,7 @@ impl ReaderApp {
         ))
     }
 
+    /// Create an application with all the required data
     fn create(
         ctx: Context,
         img_source: Box<dyn ImageSource>,
@@ -75,35 +95,61 @@ impl ReaderApp {
         let threads_stop_signal = Arc::new(AtomicBool::new(false));
         let current_page = Arc::new(AtomicUsize::new(0));
 
+        // We collect here the list of all threads that we'll need to close when e.g.
+        // loading another file
         let mut thread_handles = vec![];
 
-        let threads_count = *LOGICAL_CORES;
+        // How many loading threads to use
+        let threads_count = std::cmp::min(*LOGICAL_CORES, 16);
 
+        // Create the loading threads
         for thread_num in 0..threads_count {
             let mut img_source = img_source.quick_clone();
 
-            // let path = path.clone();
             let ctx = ctx.clone();
             let thread_stop_signal = Arc::clone(&threads_stop_signal);
             let loaded_pages = Arc::clone(&loaded_pages);
             let current_page = Arc::clone(&current_page);
 
+            // Each thread loads a part of the pages, depending on its number
+            // The loaded pages are (total_threads * n) + thread_number
+            //
+            // For instance, given 8 threads:
+            // Thread n°4 will load pages 4, 12, 20, etc.
+            // Thread n°6 will load pages 6, 14, 22, etc.
             thread_handles.push(std::thread::spawn(move || {
+                // We setup the pages to load here, this is useful when changing priorities below
                 let mut pages_to_load = (0..total_pages).filter(|i| i % threads_count == thread_num).collect::<Vec<_>>();
 
+                // Load remaining pages
                 while !pages_to_load.is_empty() {
+                    // The priority is always to load the pages the user is looking at first,
+                    // and then the next ones in the image set.
+                    // So before loading a page, we always get the first one greater than or equal to
+                    // the current one.
                     let prioritize_loading_from = current_page.load(Ordering::Acquire);
 
+                    // We get the index of the page index in the list...
                     let page_index_in_vec = pages_to_load.iter().position(|i| *i >= prioritize_loading_from).unwrap_or(0);
+
+                    // ...to remove it and retrieve it
                     let page = pages_to_load.remove(page_index_in_vec);
 
+                    // We load the image from the source
                     let img = img_source.load_page(page);
 
+                    // Then we save it to the list of loaded pages
+                    // Note that the lock is acquired in a single condition, meaning the lock
+                    // is dropped immediatly after the writing
                     loaded_pages.write().unwrap().set(page, img);
 
+                    // Request a repaint (will trigger the UI update function to take
+                    // into account the fact we now have new pages data available)
                     ctx.request_repaint();
-                    
+
+                    // If the application indicates it's trying to stop...
                     if thread_stop_signal.load(Ordering::Acquire) {
+                        // Just quit the thread!
                         return;
                     }
                 }
@@ -126,16 +172,20 @@ impl ReaderApp {
         }
     }
 
+    /// Load a new file or directory
     fn load_path(&mut self, path: PathBuf) -> Result<()> {
+        // Load the image source (to ensure it's valid)
         let img_source = load_image_source(&path)?;
 
+        // Then indicate all threads they must stop as soon as possible
         self.threads_stop_signal.store(true, Ordering::Release);
 
-        
+        // Wait for all threads to finish properly
         while let Some(thread_handle) = self.thread_handles.pop() {
             thread_handle.join().map_err(|_| anyhow!("Internal error: failed to join thread"))?;
         }
 
+        // Then re-create the application (which will set up new threads)
         *self = Self::create(
             self.ctx.clone(),
             img_source,
@@ -146,22 +196,33 @@ impl ReaderApp {
         Ok(())
     }
 
+    /// Jump to a neighbour file
     fn relative_file_change(&mut self, relative: isize) -> Result<()> {
+        assert!(relative == -1 || relative == 1);
+
+        // If there is no open file, we cannot get the list of neighbour ones
+        // So we don't do anything
         let Some(path) = &self.path else {
             return Ok(());
         };
 
+        // Same goes if the opened file doesn't have a parent
+        // (e.g. we opened the root directory)
         let Some(parent) = path.parent() else {
             return Ok(())
         };
 
+        // Get all items in the current file's parent directory
         let items = fs::read_dir(parent)?.collect::<Result<Vec<_>, _>>()?;
 
+        // Find it in the list
+        // Note that it may have been moved between the moment it was opened and now
         let index = items
             .iter()
             .position(|c| &c.path() == path)
             .context("File not found in parent directory")?;
 
+        // Check if we can do the jump
         if -relative > isize::try_from(index).unwrap() {
             bail!("No previous file in parent directory");
         }
@@ -172,10 +233,14 @@ impl ReaderApp {
             bail!("No next file in parent directory");
         }
 
+        // Jump!
         self.load_path(items[index].path())
     }
 
+    /// Perform a relative page change
     fn relative_page_change(&mut self, mut inc: isize, shift: bool) {
+        assert!(inc == -1 || inc == 1);
+
         let settings = self.settings.read().unwrap();
 
         if settings.double_page && !shift {
@@ -203,6 +268,7 @@ impl ReaderApp {
         }
     }
 
+    /// Handle inputs (keyboard, mouse, etc.) from the UI thread
     fn handle_inputs(&mut self, i: &InputState) {
         if i.key_pressed(Key::Home) {
             self.current_page.store(0, Ordering::Release);
@@ -282,6 +348,7 @@ impl ReaderApp {
         }
     }
 
+    /// Handle file drops from other applications
     fn handle_file_drops(&mut self, i: &InputState) {
         let files = &i.raw.dropped_files;
 
@@ -304,6 +371,7 @@ impl ReaderApp {
         }
     }
 
+    /// Compute a displayable image for a given page
     fn compute_displayable_page(&self, page: usize) -> Result<Option<(TextureHandle, Vec2)>, String> {
         let started = Instant::now();
 
@@ -327,21 +395,29 @@ impl ReaderApp {
 
 impl eframe::App for ReaderApp {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        // Save settings
         eframe::set_value(storage, eframe::APP_KEY, &*self.settings.read().unwrap());
     }
 
+    // The main rendering function, which computes the UI in immediate mode
     fn update(&mut self, ctx: &Context, frame: &mut eframe::Frame) {
+        // We first need a central panel to display everything inside
         CentralPanel::default()
             .frame(Frame::none())
             .show(ctx, |ui| {
+                // We start by handling user inputs
+                // this may impact the current page number, opened file, etc.
                 ctx.input(|i| {
                     self.handle_inputs(i);
                     self.handle_file_drops(i);
                 });
 
+                // Get the current window's size (required to scale the pages properly)
                 let win_size = frame.info().window_info.size;
 
+                // If the "jump to page" modal is opened...
                 if self.page_prompt.is_some() {
+                    // Show it!
                     Window::new("Jump to page")
                         .pivot(Align2::CENTER_CENTER)
                         .default_pos((win_size / 2.0).to_pos2())
@@ -375,6 +451,7 @@ impl eframe::App for ReaderApp {
                         });
                 }
 
+                // Render a given page in the UI, synchronously
                 let render_page = |ui: &mut Ui, page: usize| {
                     if page >= self.total_pages {
                         ui.label(" "); // Empty widget
@@ -418,6 +495,7 @@ impl eframe::App for ReaderApp {
 
                 let current_page = self.current_page.load(Ordering::Acquire);
 
+                // Determine the pages to render and render them
                 let pages = if self.total_pages == 0 {
                     ui.heading("Nothing to display");
                     
@@ -429,13 +507,18 @@ impl eframe::App for ReaderApp {
 
                     (Some(current_page), None)
                 } else {
+                    // We remove any space between columns to get a gapless display in double mode
                     ui.spacing_mut().item_spacing = Vec2::ZERO;
+
                     ui.columns(2, |columns| {
                         let (left_page, right_page) = if settings.right_to_left {
                             (current_page + 1, current_page)
                         } else {
                             (current_page, current_page + 1)
                         };
+
+                        // Using a two-columns layout allows to use custom alignemnt
+                        // for each of them
 
                         columns[0].with_layout(
                             Layout::right_to_left(Align::Center),
@@ -455,6 +538,7 @@ impl eframe::App for ReaderApp {
                     (Some(current_page), Some(current_page + 1))
                 };
 
+                // Display the pages number if enabled in the settings
                 if settings.display_pages_number {
                     Area::new("pages_number")
                         .anchor(Align2::RIGHT_TOP, Vec2::ZERO)
